96b9a252f5add7d595dc36113725147b2362826b
Isabela Lima <isabelamslimas@gmail.com>
2022-05-17 14:23:02 -0300

    adding services and nested components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
@@ -14,48 +15,36 @@ export class ProductListComponent implements OnInit {
 
   private _listFilter: string = '';
   get listFilter(): string {
-      return this._listFilter;
+    return this._listFilter;
   }
   set listFilter(value: string) {
-      this._listFilter = value;
-      this.filteredProducts = this.performFilter(value);
+    this._listFilter = value;
+    console.log('In setter: ' + value);
+    this.filteredProducts = this.performFilter(value);
   }
 
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-  ];
+  products: IProduct[] = [];
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
+  constructor(private productService: ProductService) {}
+
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
-      product.productName.toLocaleLowerCase().includes(filterBy));
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
   }
 
   ngOnInit(): void {
-      this._listFilter = 'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
   }
 }
